f301c024ff0a1000688f8dac98bd8f937f151bcc
Sukalyan14 <sukalyannayak@gmail.com>
2023-10-25 23:53:03 +0530

    trying concurrently having issues with it, so skipping it for now

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 // Packages , imports and ports
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan')
+
 const router = require('./routes/route.js');
-const allowed_origins = ['http://localhost:1234'];
 require('dotenv').config();
+
+const allowed_origins = ['http://localhost:1234'];
 const port = process.env.PORT || 3300;
 
 // Middleware
 const app = express();
 
 app.use(express.json());
+
+app.use(morgan('tiny'))
 
 app.use(cors({
   origin: allowed_origins[0],
