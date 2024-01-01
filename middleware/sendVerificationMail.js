22588d7160bcccd2710a0ee458a09155ea1658bb
Sukalyan14 <sukalyannayak@gmail.com>
2024-01-01 23:34:13 +0530

    Ok , so all foresable scenarios regarding registration token gen has been taken care of, As off now need to refocus back on verification button from email and its check. though need to clean sendVerification later to prevent the repeat of same code at multiple instances

--- middleware/sendVerificationMail.js
+++ middleware/sendVerificationMail.js
@@ -10,103 +10,45 @@ const RegisterCheckCluster0 = require('../models/register_verify');
 // Need to prevent multiple entries of the same email in the db
 async function sendVerificationEmail(emailId , password , duration){
     try{
-        // console.log("hello");
         const saltRounds = 10
         const datetime_current = new Date();
-        // const time = `${d.getHours()}:${d.getMinutes()}`
         const time = datetime_current.toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric"})
-        // console.log(time);
-        // RegisterCheckCluster0.find({
-        //     email:emailId
-        // }).then(result => {
-        //     if(result){
-        //         let { date , time_stamp } = result[0]
-        //         console.log(date);
-        //         console.log(datetime_current);
-        //         // const datetime_current = new Date()
-        //         // console.log(date.getHours() , date.getMinutes() , time_stamp);
-        //         // console.log(datetime_current.getHours() , datetime_current.getMinutes());
-        //         console.log((datetime_current - date)/(1000*60));
-        //         // const datetime_current = new Date().toLocaleDateString();
-        //         const current_time =  new Date().toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric"});
-        //         if(datetime_current === date){
-        //             console.log("same dates");
-        //             console.log(current_time , time_stamp);
-        //             let diff = (datetime_current - date)/(1000*60)
-        //             // console.log(diff);
-        //         }
-        //         // console.log(d2 , date2 , time_stamp   , current_time);
-        //     }
-        // })
-        // Check if mail exists
+
+        //smtp config details
+        let config = {
+            service : process.env.MAIL_SERVICE_NAME,
+            port: process.env.MAIL_SERVICE_PORT, // or the appropriate port for your SMTP server
+            secure: process.env.MAIL_SERVICE_SECURE, // true for 465, false for other ports
+            auth:{
+                user :process.env.MAIL_USER_NAME,
+                pass :process.env.MAIL_USER_PASSWORD
+            }
+        }
+
+        let nodemailerTransporter = nodemailer.createTransport(config)
+    
+        let options = {
+            viewEngine: {
+                extname: '.handlebars',
+                layoutsDir: './emailTemplates',
+                partialsDir: './emailTemplates',
+                defaultLayout: 'emailVerifications'
+            },
+            viewPath: './emailTemplates',
+            extname: '.handlebars'
+        }
+
+        nodemailerTransporter.use('compile' , hbs(options))
+
+        // Check if mail exists in db
         let mail_register_check = await RegisterCheckCluster0.find({
             email: emailId,
-            // verified:true
         })
         
-        //Check time duration with the last update
-        if (mail_register_check && mail_register_check[0].verified === false){
-            if ((datetime_current - mail_register_check[0].date)/(1000*60) > duration){
-                const current_date = (new Date()).valueOf().toString();
-                let random = Math.random().toString();
-                let token = crypto.createHash('MD5').update(current_date + random).digest('hex');
-            
-                let verificationLink = process.env.VERIFY_END_POINT_URL+"?key="+token;  //Store in db for a specific user-session
-
-                const update_data = await RegisterCheckCluster0.findOneAndUpdate({ email:emailId } , {
-                    session_token : token ,
-                    date:datetime_current,
-                    time_stamp : time ,
-                })  //Returns the data pre-update
-                // Rest of the process of mail goes here
-                console.log(update_data);
-            }
-        } else if(mail_register_check && mail_register_check[0].verified === true) {
-            return {
-                // info,
-                message: 'Email Already Registered'
-            }
-        } else {
-            console.log("Email not present in db regular process");
-        }
-        
-        let mail_register_checks = []
-        // console.log(mail_register_check);
-        
-        //Checking if mail is already registered or not
-        if (mail_register_checks.length === 0){
-            
-            const current_date = (new Date()).valueOf().toString();
-            let random = Math.random().toString();
-            let token = crypto.createHash('MD5').update(current_date + random).digest('hex');
-            
-            let verificationLink = process.env.VERIFY_END_POINT_URL+"?key="+token;  //Store in db for a specific user-session
-    
-            let config = {
-                service : process.env.MAIL_SERVICE_NAME,
-                port: process.env.MAIL_SERVICE_PORT, // or the appropriate port for your SMTP server
-                secure: process.env.MAIL_SERVICE_SECURE, // true for 465, false for other ports
-                auth:{
-                    user :process.env.MAIL_USER_NAME,
-                    pass :process.env.MAIL_USER_PASSWORD
-                }
-            }
-    
-            let nodemailerTransporter = nodemailer.createTransport(config)
-    
-            let options = {
-                viewEngine: {
-                    extname: '.handlebars',
-                    layoutsDir: './emailTemplates',
-                    partialsDir: './emailTemplates',
-                    defaultLayout: 'emailVerifications'
-                },
-                viewPath: './emailTemplates',
-                extname: '.handlebars'
-            }
-    
-            nodemailerTransporter.use('compile' , hbs(options))
-
+        //Mail Does Not Exists
+        if(mail_register_check && mail_register_check.length === 0){
+            //Generate token put the mail in the docs
+            let { token , verificationLink } = generate_token()
 
             // Need to put proper checks that db entry is successful and mail is also sent , a scenario where 1 happens and other fails is to be prevented
             const info = await nodemailerTransporter.sendMail({
@@ -121,23 +63,85 @@ async function sendVerificationEmail(emailId , password , duration){
             });
             
             if (info.accepted.length > 0){
-                // bcrypt.hash(password , saltRounds , async function(err , hash) {
-                //     const register_data = await RegisterCheckCluster0.create({
-                //         email : emailId ,
-                //         session_token : token ,
-                //         password : hash , 
-                //         date:datetime_current,
-                //         time_stamp : time ,
-                //         verified : false
-                //     })
-                // })
+                bcrypt.hash(password , saltRounds , async function(err , hash) {
+                    const register_data = await RegisterCheckCluster0.create({
+                        email : emailId ,
+                        session_token : token ,
+                        password : hash , 
+                        date:datetime_current,
+                        time_stamp : time ,
+                        verified : false
+                    })
+                })
                 return {
                     info,
                     message: 'Email Sent Successfully'
                 }
             } else {
                 throw new Error('Mail Not Sent')
-            }            
+            }
+        }
+        //If mail exists
+        else if(mail_register_check && mail_register_check.length !== 0 && mail_register_check[0].verified === false){
+            console.log(duration);
+            // If greater than duration generate token & update timings
+            if ((datetime_current - mail_register_check[0].date)/(1000*60) >= duration){
+                // console.log((datetime_current - mail_register_check[0].date)/(1000*60) , duration);
+                let { token , verificationLink } = generate_token()
+
+                const info = await nodemailerTransporter.sendMail({
+                    from: process.env.ADMIN_MAIL,
+                    to:emailId,
+                    subject:'Email Verification',
+                    template: 'emailVerifications',
+                    context:{
+                        logoUrl : process.env.LOGO_URL,
+                        verificationLink: verificationLink ,
+                    }
+                });
+                
+                if (info.accepted.length > 0){
+                    const update_data = await RegisterCheckCluster0.findOneAndUpdate({ email:emailId } , {
+                        session_token : token ,
+                        date:datetime_current,
+                        time_stamp : time ,
+                    })  //Returns the data pre-update
+                                                                                                        
+                    return {
+                        info,
+                        message: 'Email Sent Successfully'
+                    }
+                } else {
+                    throw new Error('Mail Not Sent')
+                }
+
+            } else {
+                //Just send mail donot generate token again
+                const info = await nodemailerTransporter.sendMail({
+                    from: process.env.ADMIN_MAIL,
+                    to:emailId,
+                    subject:'Email Verification',
+                    template: 'emailVerifications',
+                    context:{
+                        logoUrl : process.env.LOGO_URL,
+                        verificationLink: process.env.VERIFY_END_POINT_URL+"?key="+mail_register_check[0].session_token ,
+                    }
+                });
+
+                if(info.accepted.length > 0){
+                    return {
+                        info,
+                        message: 'Email Sent Successfully'
+                    }
+                } else {
+                    throw new Error('Mail Not Sent')
+                }
+
+            }
+        } else if(mail_register_check && mail_register_check.length !== 0 && mail_register_check[0].verified === true){
+            return {
+                message: "Email Already Registered"
+            }
         }
     }
     
@@ -151,7 +155,14 @@ async function sendVerificationEmail(emailId , password , duration){
 }
 
 const generate_token = () => {
-    
+    const current_date = (new Date()).valueOf().toString();
+    let random = Math.random().toString();
+            
+    let token = crypto.createHash('MD5').update(current_date + random).digest('hex') 
+
+    let verificationLink = process.env.VERIFY_END_POINT_URL+"?key="+token;
+
+    return { token , verificationLink }
 }
 
 module.exports = { sendVerificationEmail }
